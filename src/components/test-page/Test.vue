caac950f0b9438f60f581b1a7640d0bddcb3bf85
DingoPham <pencibambo@gmail.com>
2024-06-18 21:01:55 +0700

    test các components

---
+++ src/components/test-page/Test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        
+        <p> {{ data_test }}</p> <!-- data_test: nhận dữ liệu từ trang cha vào -->
+        <button @click="click_me">Click me</button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    // props: những dữ liệu từ component 
+    // cha truyền vào componet con
+    // comp cha: Admin
+    // comp con: Test
+    props: ['data_test','checker'],
+    // Watch hàm giám sát thay đổi trong một thuộc
+    // tính phản ứng và phản hồi tương ứng
+    watch:{
+        checker(newData){
+            console.log(newData)
+        }
+    },
+    methods:{
+        click_me: function(){
+            let me = this;
+            me.$emit("click_test", "test 1234")
+        },
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,64 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// authentication
+import Authentication from '@/components/authentication/Authentication'
+import Login from '@/components/authentication/login/Login'
+import ForgetPassword from '@/components/authentication/forget-password/ForgetPassword'
+ 
+// admin
+import Admin from '@/components/Admin'
+
+
+import Account from '@/components/Account'
+import Product from '@/components/Product'
+import Order from '@/components/Order'
+import { name } from 'file-loader'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: Account,
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: Product,
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order,
+    },
+    {
+      path: '/authentication',
+      name: 'authentication',
+      component: Authentication,
+      meta:{
+        isAuthentication: true
+      },
+      children: [
+        {
+          path: "login",
+          name: "login",
+          component: Login
+        },
+        {
+          path: "forget-password",
+          name: "forgetpassword",
+          component: ForgetPassword
+        }
+      ]
     }
-  ]
+  ] 
 })
